b0213af47247afef8c8ca2d0a69369d8635106f7
Alice131313 <131433241+Alice131313@users.noreply.github.com>
2023-05-12 09:22:47 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,13 @@
-
+radio.onReceivedNumber(function (receivedNumber) {
+    number = radio.receivedPacket(RadioPacketProperty.SignalStrength)
+    led.plotBarGraph(
+    Math.map(number, -95, -42, 0, 9),
+    9
+    )
+})
+let number = 0
+radio.setGroup(1)
+radio.setTransmitPower(5)
+basic.forever(function () {
+    radio.sendNumber(1)
+})
